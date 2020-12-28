29cbdc8728f4b809da2fe5a0e3e3831bdb88c93c
VideFrelan <76523793+VideFrelan@users.noreply.github.com>
2020-12-28 18:02:05 +0700

    Update heker.php

--- heker.php
+++ heker.php
@@ -8,7 +8,7 @@
 <body>
 <div align="center">
 <h1>Heker nih pasti...</h1>
-<font color="white">Dapat salam dari [isonsix]</font>
+<font color="white">Dapat salam dari [PawN0]</font>
 </div>
 </body>
 </html>
